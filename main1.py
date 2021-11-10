29102cd469cb82929bfa8c461006ab2dc75d7871
STUDENT <kaggasravani2001@gmail.com>
2021-11-10 10:32:03 +0530

    print statement was updated

--- main1.py
+++ main1.py
@@ -2,6 +2,6 @@ import math
 number=int(input("enter the number"))
 root=math.sqrt(number)
 if int(root+0.5)**2==number:
-	print(number,"is a perfect square ")
+	print("given number is a perfect square ")
 else:
 	print(number,"is not a perfect square")
